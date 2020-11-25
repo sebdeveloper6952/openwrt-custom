db5f1bb4cd4f944100955388eae4fa4ebf5ba8c7
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-11-25 10:07:04 -0600

    g.

--- run_config.sh
+++ run_config.sh
@@ -1,23 +1,15 @@
+# copy custom configuration files
 mv /etc/custom/openwrt-system /etc/config/system
 mv /etc/custom/openwrt-network /etc/config/network
-/etc/init.d/network reload
 mv /etc/custom/openwrt-dhcp /etc/config/dhcp
+mv /etc/custom/openwrt-firewall /etc/config/firewall
+
+# reload services to take as input the new config files
+/etc/init.d/network reload
 /etc/init.d/dnsmasq reload
+/etc/init.d/firewall reload
 
 # ntp
 /etc/init.d/sysntpd disable
 /etc/init.d/ntpd enable
 /etc/init.d/ntpd start
-
-# use google dns-servers
-uci -q delete dhcp.@dnsmasq[0].server
-uci add_list dhcp.@dnsmasq[0].server="8.8.8.8"
-uci add_list dhcp.@dnsmasq[0].server="8.8.4.4"
-uci set dhcp.@dnsmasq[0].noresolv="0"
-uci commit dhcp
-/etc/init.d/dnsmasq restart
-
-# firewall
-mv /etc/custom/openwrt-firewall /etc/config/firewall
-# /etc/init.d/firewall reload
-# /etc/init.d/firewall restart
