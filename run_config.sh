7daa29e3569ed0c8d16b9baa8b5f568bd357595d
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-11-24 22:21:01 -0600

    a.

--- run_config.sh
+++ run_config.sh
@@ -13,13 +13,28 @@ mv /etc/custom/openwrt-dhcp /etc/config/dhcp
 uci -q delete dhcp.@dnsmasq[0].server
 uci add_list dhcp.@dnsmasq[0].server="8.8.8.8"
 uci add_list dhcp.@dnsmasq[0].server="8.8.4.4"
-uci set dhcp.@dnsmasq[0].noresolv="1"
+uci set dhcp.@dnsmasq[0].noresolv="0"
 uci commit dhcp
 /etc/init.d/dnsmasq restart
 
+# Configure DNS provider
+uci -q delete network.wan.dns
+uci add_list network.wan.dns="8.8.8.8"
+uci add_list network.wan.dns="8.8.4.4"
+ 
+# Configure DNS6 provider
+uci -q delete network.wan6.dns
+uci add_list network.wan6.dns="2001:4860:4860::8888"
+uci add_list network.wan6.dns="2001:4860:4860::8844"
+ 
+# Disable peer DNS
+uci set network.wan.peerdns="0"
+uci set network.wan6.peerdns="0"
+ 
+# Save and apply
+uci commit network
+/etc/init.d/network restart
+
 # firewall
-uci set firewall.@forwarding[1]=forwarding
-uci set firewall.@forwarding[1].src='wan'
-uci set firewall.@forwarding[1].dest='lan'
-uci commit firewall
-/etc/init.d/firewall reload
+mv /etc/custom/openwrt-firewall /etc/config/firewall
+/etc/init.d/firewall restart
