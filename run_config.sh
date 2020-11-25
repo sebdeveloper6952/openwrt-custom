5d29726f70226eed42403ee5888173721bf5880e
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-11-24 22:42:17 -0600

    b.

--- run_config.sh
+++ run_config.sh
@@ -17,24 +17,6 @@ uci set dhcp.@dnsmasq[0].noresolv="0"
 uci commit dhcp
 /etc/init.d/dnsmasq restart
 
-# Configure DNS provider
-uci -q delete network.wan.dns
-uci add_list network.wan.dns="8.8.8.8"
-uci add_list network.wan.dns="8.8.4.4"
- 
-# Configure DNS6 provider
-uci -q delete network.wan6.dns
-uci add_list network.wan6.dns="2001:4860:4860::8888"
-uci add_list network.wan6.dns="2001:4860:4860::8844"
- 
-# Disable peer DNS
-uci set network.wan.peerdns="0"
-uci set network.wan6.peerdns="0"
- 
-# Save and apply
-uci commit network
-/etc/init.d/network restart
-
 # firewall
 mv /etc/custom/openwrt-firewall /etc/config/firewall
 /etc/init.d/firewall restart
